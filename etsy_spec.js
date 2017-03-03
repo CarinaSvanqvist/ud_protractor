824fdcf6a2e0b4d037a2bd2dbc8d396b8ac1b0bc
Carina Svanqvist <carina.svanqvist@gmail.com>
2017-03-03 15:51:26 +0100

    Updated file with faker library to get random users when try to register account

--- etsy_spec.js
+++ etsy_spec.js
@@ -1,4 +1,5 @@
 var helper = require('C:/Users/Carina/udemy-protractor/helper.js')
+var faker = require('faker')
 var util = require('util')
 describe("Ëtsy MainPaige Register, Sign in", function () {
 
@@ -11,16 +12,20 @@ describe("Ëtsy MainPaige Register, Sign in", function () {
         browser.manage().deleteAllCookies();
     })
 
+    var randomFirstName = faker.name.firstName();
+    var randomLastName = faker.name.lastName();
+    var email = faker.internet.email();
+    var password = faker.internet.password();
     it('1. Should find the title on the Page', function () {
         var title = 'Etsy.com | Shop for anything from creative people everywhere';
         expect(browser.getTitle()).toEqual(title);
     })
     it('2. Should register a new useraccount', function () {
-        var firstname = 'Test';
-        var surname = 'Test';
-        var email = 'tester@mail.com';
-        var pw = 'Password';
-        var succeedReg;
+        //var firstname = randomFirstName;
+        //var surname = randomLastName;
+        //var email = email;
+        //var pw = password;
+        //var succeedReg;
 
         var firstnameField = element(by.id('first-name'));
         var surnameField = element(by.id('last-name'));
@@ -30,11 +35,11 @@ describe("Ëtsy MainPaige Register, Sign in", function () {
 
         element(by.id('register')).click();
         helper.waitUntilReady(firstnameField);
-        firstnameField.sendKeys(firstname);
-        surnameField.sendKeys(surname);
+        firstnameField.sendKeys(randomFirstName);
+        surnameField.sendKeys(randomLastName);
         emailField.sendKeys(email);
-        passwordField.sendKeys(pw);
-        confirmPwField.sendKeys(pw);
+        passwordField.sendKeys(password);
+        confirmPwField.sendKeys(password);
         /* Test does not register a real account
     //    element(by.id('register_button')).click();
     //    helper.waitUntilReady(succeedReg);
